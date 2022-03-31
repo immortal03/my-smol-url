4f070a6fe8a90b3a2958bfd35e76021281eacdc1
immortal03 <michaelnch91@gmail.com>
2022-04-01 00:51:46 +0800

    usePageTitle hook returns setter method

--- app/javascript/hooks/usePageTitle.js
+++ app/javascript/hooks/usePageTitle.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { Helmet } from "react-helmet"
 
 const usePageTitle = (pageTitle = "My Smol URL") => {
   if (!pageTitle) throw new Error("You must provide a page title")
 
-  return () => (
-    <Helmet>
-      <title>{pageTitle}</title>
-    </Helmet>
-  )
+  const [title, setTitle] = useState(pageTitle)
+
+  const PageTitle = () => {
+    return (
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+    )
+  }
+
+  return { PageTitle, setTitle }
 }
 
 export default usePageTitle

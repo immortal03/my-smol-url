776b3dce0d13cbb36eddbcf113157f6c0e24deee
immortal03 <michaelnch91@gmail.com>
2022-04-02 16:40:36 +0800

    Refactor record click event service

--- app/services/event_handler/record_click_event.rb
+++ app/services/event_handler/record_click_event.rb
@@ -1,11 +1,6 @@
 class EventHandler::RecordClickEvent < ApplicationService
   attr_reader :link_id, :ip, :user_agent, :event_at
 
-  # Sample data for dev env usage
-  SAMPLE_COUNTRIES = %w[Singapore Malaysia Indonesia Thailand China Australia].freeze
-  SAMPLE_DEVICES = ["iPhone 13", "Galaxy S22", "iPhone 12 Pro Max", "Huawei P30", "OnePlus 10", "Nokia 3380"].freeze
-  SAMPLE_BROWSERS = ["Chrome", "Firefox", "Safari", "Edge", "Opera", "Internet Explorer"].freeze
-
   def initialize(link_id:, ip:, user_agent:, event_at:)
     raise ArgumentError unless link_id.present? && ip.present? && user_agent.present? && event_at.present?
 
@@ -25,7 +20,8 @@ class EventHandler::RecordClickEvent < ApplicationService
 
       # Get IP info using geocoder
       # https://github.com/alexreisner/geocoder
-      geolocation_data = begin
+      # 5 minutes caching in case transaction fails / dup and rerun
+      geolocation_data = Rails.cache.fetch("/geocoder/#{@ip}", expires_in: 5.minutes) do
         results = Geocoder.search(@ip)
         result = results.first
 
@@ -44,7 +40,6 @@ class EventHandler::RecordClickEvent < ApplicationService
       end
 
       click_event.assign_attributes(browser_data.merge(geolocation_data))
-
       click_event.save!
     end
   end

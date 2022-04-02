7516d13a5f9c3a53ce718d907cd967c0534d7111
immortal03 <michaelnch91@gmail.com>
2022-04-02 16:41:24 +0800

    Graphql queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,14 @@
 module Types
   class QueryType < Types::BaseObject
-    field :retrieve_links_with_connection, Link::LinkType.connection_type, null: true, connection: true
+    field :retrieve_links_with_connection, Link::LinkType.connection_type, null: false, connection: true
 
     field :retrieve_link, Link::LinkType, null: true do
       argument :slug, String, required: true
+    end
+
+    field :retrieve_chart_clicks_by_days, GraphQL::Types::JSON, null: true do
+      argument :slug, String, required: true
+      argument :days, Integer, required: false
     end
 
     def retrieve_links_with_connection
@@ -12,6 +17,10 @@ module Types
 
     def retrieve_link(slug:)
       Link.find_by(slug: slug)
+    end
+
+    def retrieve_chart_clicks_by_days(slug:, days: 30)
+      Link.find_by(slug: slug)&.chart_clicks_by_days(days: days)
     end
   end
 end
--- test/graphql/link_queries_test.rb
+++ test/graphql/link_queries_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class LinkQueriesTest < ActiveSupport::TestCase
-  test "retrieveLink query should retrieve link by slug" do
-    query_string = <<~GRAPHQL
+  def setup
+    @retrieve_link_query = <<~GRAPHQL
       query($slug: String!) {
       retrieveLink(slug: $slug) {
           id
@@ -10,17 +10,7 @@ class LinkQueriesTest < ActiveSupport::TestCase
       }
     GRAPHQL
 
-    slug = "a-new-slug"
-    link = Link.create(url: "https://example.com", slug: slug)
-    response = MySmolUrlSchema.execute(query_string, variables: {
-      slug: slug
-    })
-
-    assert_equal link.id, response.dig("data", "retrieveLink", "id")
-  end
-
-  test "retrieveLinksWithConnection query should retrieve paginate links" do
-    query_string = <<~GRAPHQL
+    @retrieve_links_with_connection_query = <<~GRAPHQL
       query($first: Int = 10, $after: String) {
         retrieveLinksWithConnection(first: $first, after: $after) {
           pageInfo {
@@ -34,10 +24,56 @@ class LinkQueriesTest < ActiveSupport::TestCase
       }
     GRAPHQL
 
+    @retrieve_chart_clicks_by_days_query = <<~GRAPHQL
+      query($slug: String!, $days: Int){
+        retrieveChartClicksByDays(slug: $slug, days: $days)
+      }
+    GRAPHQL
+  end
+
+  test "retrieveLink query should retrieve link by slug" do
+    slug = "a-new-slug"
+    link = Link.create(url: "https://example.com", slug: slug)
+    response = MySmolUrlSchema.execute(@retrieve_link_query, variables: {
+      slug: slug
+    })
+
+    assert_equal link.id, response.dig("data", "retrieveLink", "id")
+  end
+
+  test "retrieveLink query should return nil if invalid slug provided" do
+    response = MySmolUrlSchema.execute(@retrieve_link_query, variables: {
+      slug: "an-invalid-slug-123"
+    })
+
+    assert response.dig("data", "retrieveLink", "id").nil?
+  end
+
+  test "retrieveLinksWithConnection query should retrieve paginate links" do
     Link.create(url: "https://example.com", slug: "a-new-slug")
-    response = MySmolUrlSchema.execute(query_string)
+    response = MySmolUrlSchema.execute(@retrieve_links_with_connection_query)
 
     assert response.dig("data", "retrieveLinksWithConnection").key?("pageInfo")
     assert response.dig("data", "retrieveLinksWithConnection").key?("nodes")
+  end
+
+  test "retrieveChartClicksByDays query should retrieve grouped clicks count by day" do
+    Link.create(url: "https://example.com", slug: "a-new-slug")
+    response = MySmolUrlSchema.execute(@retrieve_chart_clicks_by_days_query, variables: {
+      slug: "a-new-slug",
+      days: 30
+    })
+
+    assert response.dig("data", "retrieveChartClicksByDays").key?(:labels)
+    assert response.dig("data", "retrieveChartClicksByDays").key?(:data)
+  end
+
+  test "retrieveChartClicksByDays query should return nil if invalid slug provided" do
+    response = MySmolUrlSchema.execute(@retrieve_chart_clicks_by_days_query, variables: {
+      slug: "an-invalid-slug-123",
+      days: 30
+    })
+
+    assert response.dig("data", "retrieveChartClicksByDays").nil?
   end
 end

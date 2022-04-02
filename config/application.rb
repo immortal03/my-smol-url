503c90f553939d095edf0740d34018f94082ed04
immortal03 <michaelnch91@gmail.com>
2022-04-02 16:42:50 +0800

    Application config changes

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,12 @@ module MySmolUrl
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Singapore"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_job.queue_adapter = :sidekiq
+
+    # Use Rack::Attack for rate limiting, client whitelisting, etc.
+    config.middleware.use Rack::Attack
   end
 end
